c5e1e4484d56c0b4cf390f942bf705b65530cc96
krishangoyal12 <krishangoyal717@gmail.com>
2025-03-17 10:28:38 +0530

    Created Navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import {Route, Routes} from 'react-router-dom'
 import Home from './Pages/Home'
+import { Navbar } from './Components/Navbar'
 
 function App() {
 
 
   return (
     <>
+    <Navbar/>
       <Routes>
         <Route path = '/' element = {<Home/>}/>
       </Routes>
